cbd6f086874aa137b11210bfdc59a61aded9b48c
OhioIon <->
2018-11-07 19:56:02 +0100

    Prevent project is recreated in own sub-folder

--- _fpga/vivado/export.tcl
+++ _fpga/vivado/export.tcl
@@ -2,4 +2,4 @@
 set prjDir [get_property DIRECTORY [current_project]]
 
 # Export project.tcl which can be used to recreate the project in Vivado
-write_project_tcl -force -all_properties $prjDir/project.tcl
+write_project_tcl -force -all_properties -target_proj_dir . $prjDir/project.tcl
