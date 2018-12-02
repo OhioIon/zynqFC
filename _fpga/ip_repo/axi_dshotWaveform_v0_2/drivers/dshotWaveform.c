f5a5f17650459d3fca588b00aa605e90a1de4fcc
OhioIon <->
2018-12-02 14:14:16 +0100

    Fixed dshotWaveform driver
    - Will not continue initialization in case clock frequency register is 0
    - Fixed address offset of clock register
    
    /// DSHOT + DSHOTWAVEFORM successfully tested on Zybo devboard with Wraith 32 ESC (BLheli32) at DShot600 speed ///

--- _fpga/ip_repo/axi_dshotWaveform_v0_2/drivers/dshotWaveform.c
+++ _fpga/ip_repo/axi_dshotWaveform_v0_2/drivers/dshotWaveform.c
@@ -5,6 +5,10 @@
 #include "dshotWaveform.h"
 
 /******************* Defines ********************/
+#define REG0   0
+#define REG1   4
+#define REG2   8
+#define ENABLE 0x80000000
 
 /******************** Types *********************/
 
@@ -36,27 +40,34 @@ uint8_t dshotWaveform_init( dshotWaveform_ts *ps )
     if( (ps->prm_s.addrBas_pv != NULL) && (ps->prm_s.speed_kbps_u16 >= 150) && (ps->prm_s.speed_kbps_u16 <= 1200) )
     {
       // Read AXI4-lite clock frequency from reg2
-      uint32_t frqClk_Hz_u32 = RdReg32( ps->prm_s.addrBas_pv + 12 );
+      uint32_t frqClk_Hz_u32 = RdReg32( ps->prm_s.addrBas_pv + REG2 );
 
-      // Calculate total bit-time (round)
-      uint32_t cntTot_u32 = (frqClk_Hz_u32 + (uint32_t)ps->prm_s.speed_kbps_u16 * 500U) / ((uint32_t)ps->prm_s.speed_kbps_u16 * 1000U);
-
-      // Calculate high-time for bit pattern 1 (round)
-      uint32_t cntT1H_u32 = (uint32_t)(0.748503f * cntTot_u32);
-
-      // Calculate high-time for bit pattern 0 (floor)
-      uint32_t cntT0H_u32 = cntT1H_u32 / 2;
-
-      // Make some final checks
-      if( (cntT0H_u32 < cntT1H_u32) &&
-          (cntT1H_u32 < cntTot_u32) &&
-          (cntTot_u32 < 0xFFFF    ) )
+      if( frqClk_Hz_u32 != 0U )
       {
-        // Write high/low counts to reg1
-        WrReg32( ps->prm_s.addrBas_pv + 4, (cntT1H_u32 << 16) | cntT0H_u32 );
+        // Calculate total bit-time (round)
+        uint32_t cntTot_u32 = (frqClk_Hz_u32 + (uint32_t)ps->prm_s.speed_kbps_u16 * 500U) / ((uint32_t)ps->prm_s.speed_kbps_u16 * 1000U);
 
-        // Write total count to reg0 and start continuous waveform generation
-        WrReg32( ps->prm_s.addrBas_pv + 0, cntTot_u32 | 0x80000000 );
+        // Calculate high-time for bit pattern 1 (round)
+        uint32_t cntT1H_u32 = (uint32_t)(0.748503f * cntTot_u32);
+
+        // Calculate high-time for bit pattern 0 (floor)
+        uint32_t cntT0H_u32 = cntT1H_u32 / 2;
+
+        // Make some final checks
+        if( (cntT0H_u32 < cntT1H_u32) &&
+            (cntT1H_u32 < cntTot_u32) &&
+            (cntTot_u32 < 0xFFFF    ) )
+        {
+          // Write high/low counts to reg1
+          WrReg32( ps->prm_s.addrBas_pv + REG1, (cntT1H_u32 << 16) | cntT0H_u32 );
+
+          // Write total count to reg0 and enable continuous waveform generation
+          WrReg32( ps->prm_s.addrBas_pv + REG0, (cntTot_u32 - 1) | ENABLE );
+        }
+        else
+        {
+          retVal_u8++;
+        }
       }
       else
       {
