df6215a9384c35b85efe49e2ef4f1e7aaaec95eb
OhioIon <->
2019-07-10 22:47:08 +0200

    Improved driver structure and added comments.

--- _fpga/ip_repo/axi_dshotWaveform_v0_2/drivers/dshotWaveform.h
+++ _fpga/ip_repo/axi_dshotWaveform_v0_2/drivers/dshotWaveform.h
@@ -1,6 +1,24 @@
 #ifndef DSHOTWAVEFORM_H
 #define DSHOTWAVEFORM_H
 
+/** \brief "dshotWaveform" device driver
+ *
+ *  The dshotWaveform IP-core is programmed in VHDL and synthesized into the FPGA fabric.
+ *  It is connected to the CPU via AXI4-Lite bus.
+ *
+ *  Once initialized with this driver, the IP-core continuously generates
+ *  high-bit and low-bit patterns of the DShot protocol. The generated
+ *  pattern waveforms are finally used by "dshot" IP-core to send DShot
+ *  data packets to electronic speed controllers (ESCs).
+ *
+ *  The "dshotWaveform" IP-core hence controls the transmission speed (bit-rate)
+ *  of the DShot communication while the "dshot" IP-core controls the data packet
+ *  transmission and contents.
+ *
+ *  OhioIon 2019
+ *
+ */
+
 #ifdef DSHOTWAVEFORM_C
 #define DSHOTWAVEFORM_SCOPE
 #else
@@ -11,6 +29,7 @@
 
 #include <stdlib.h>
 #include <stdint.h>
+#include <stdbool.h>
 
 /******************* Defines ********************/
 
@@ -18,19 +37,22 @@
 
 typedef struct dshotWaveform_prm_s
 {
-  void *addrBas_pv;        // Physical base address of registers
-  uint16_t speed_kbps_u16; // DShot protocol speed 150 till 2400 (e.g. 600 = DShot600 = 600 kbps)
+  void     *addrBas_pv;    ///< Physical base address of IP-core registers
+  uint16_t speed_kbps_u16; ///< Desired DShot protocol speed (150 - 2400)
+  	                       ///  Example: 600 = DShot600 = 600 kbps
+
 }dshotWaveform_prm_ts;
 
 typedef struct dshotWaveform_s
 {
   dshotWaveform_prm_ts  prm_s;
+
 }dshotWaveform_ts;
 
 /****************** Public Data *****************/
 
 /******************* Interface ******************/
 
-DSHOTWAVEFORM_SCOPE uint8_t dshotWaveform_init(dshotWaveform_ts *ps );
+DSHOTWAVEFORM_SCOPE bool dshotWaveform_init(dshotWaveform_ts *ps );
 
 #endif // DSHOTWAVEFORM_H
