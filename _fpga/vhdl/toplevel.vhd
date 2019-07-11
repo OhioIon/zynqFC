6218927b26134767ee4d828c7da661cd514bdcdf
OhioIon <->
2019-07-11 23:35:57 +0200

    Added I²C peripheral to PL in order to read MPU 6050 on Zybo Board JB Pmod (upper row)

--- _fpga/vhdl/toplevel.vhd
+++ _fpga/vhdl/toplevel.vhd
@@ -11,6 +11,7 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 library UNISIM;
 use UNISIM.VCOMPONENTS.ALL;
+
 entity toplevel is
   port (
     DDR_addr : inout STD_LOGIC_VECTOR ( 14 downto 0 );
@@ -37,11 +38,14 @@ entity toplevel is
     dshot_0 : out STD_LOGIC;
     dshot_1 : out STD_LOGIC;
     dshot_2 : out STD_LOGIC;
-    dshot_3 : out STD_LOGIC
+    dshot_3 : out STD_LOGIC;
+    iic_imu_scl : inout STD_LOGIC;
+    iic_imu_sda : inout STD_LOGIC
   );
 end toplevel;
 
 architecture STRUCTURE of toplevel is
+
   component main is
   port (
     DDR_cas_n : inout STD_LOGIC;
@@ -68,36 +72,83 @@ architecture STRUCTURE of toplevel is
     dshot_0 : out STD_LOGIC;
     dshot_1 : out STD_LOGIC;
     dshot_2 : out STD_LOGIC;
-    dshot_3 : out STD_LOGIC
+    dshot_3 : out STD_LOGIC;
+    iic_imu_scl_i : in STD_LOGIC;
+    iic_imu_scl_o : out STD_LOGIC;
+    iic_imu_scl_t : out STD_LOGIC;
+    iic_imu_sda_i : in STD_LOGIC;
+    iic_imu_sda_o : out STD_LOGIC;
+    iic_imu_sda_t : out STD_LOGIC
   );
   end component main;
+  
+  component IOBUF is
+  port (
+    I : in STD_LOGIC;
+    O : out STD_LOGIC;
+    T : in STD_LOGIC;
+    IO : inout STD_LOGIC
+  );
+  end component IOBUF;
+  
+  signal iic_imu_scl_i : STD_LOGIC;
+  signal iic_imu_scl_o : STD_LOGIC;
+  signal iic_imu_scl_t : STD_LOGIC;
+  signal iic_imu_sda_i : STD_LOGIC;
+  signal iic_imu_sda_o : STD_LOGIC;
+  signal iic_imu_sda_t : STD_LOGIC;
+  
 begin
-main_i: component main
-     port map (
-      DDR_addr(14 downto 0) => DDR_addr(14 downto 0),
-      DDR_ba(2 downto 0) => DDR_ba(2 downto 0),
-      DDR_cas_n => DDR_cas_n,
-      DDR_ck_n => DDR_ck_n,
-      DDR_ck_p => DDR_ck_p,
-      DDR_cke => DDR_cke,
-      DDR_cs_n => DDR_cs_n,
-      DDR_dm(3 downto 0) => DDR_dm(3 downto 0),
-      DDR_dq(31 downto 0) => DDR_dq(31 downto 0),
-      DDR_dqs_n(3 downto 0) => DDR_dqs_n(3 downto 0),
-      DDR_dqs_p(3 downto 0) => DDR_dqs_p(3 downto 0),
-      DDR_odt => DDR_odt,
-      DDR_ras_n => DDR_ras_n,
-      DDR_reset_n => DDR_reset_n,
-      DDR_we_n => DDR_we_n,
-      FIXED_IO_ddr_vrn => FIXED_IO_ddr_vrn,
-      FIXED_IO_ddr_vrp => FIXED_IO_ddr_vrp,
-      FIXED_IO_mio(53 downto 0) => FIXED_IO_mio(53 downto 0),
-      FIXED_IO_ps_clk => FIXED_IO_ps_clk,
-      FIXED_IO_ps_porb => FIXED_IO_ps_porb,
-      FIXED_IO_ps_srstb => FIXED_IO_ps_srstb,
-      dshot_0 => dshot_0,
-      dshot_1 => dshot_1,
-      dshot_2 => dshot_2,
-      dshot_3 => dshot_3
-    );
+
+  main_i: component main
+  port map (
+    DDR_addr(14 downto 0) => DDR_addr(14 downto 0),
+    DDR_ba(2 downto 0) => DDR_ba(2 downto 0),
+    DDR_cas_n => DDR_cas_n,
+    DDR_ck_n => DDR_ck_n,
+    DDR_ck_p => DDR_ck_p,
+    DDR_cke => DDR_cke,
+    DDR_cs_n => DDR_cs_n,
+    DDR_dm(3 downto 0) => DDR_dm(3 downto 0),
+    DDR_dq(31 downto 0) => DDR_dq(31 downto 0),
+    DDR_dqs_n(3 downto 0) => DDR_dqs_n(3 downto 0),
+    DDR_dqs_p(3 downto 0) => DDR_dqs_p(3 downto 0),
+    DDR_odt => DDR_odt,
+    DDR_ras_n => DDR_ras_n,
+    DDR_reset_n => DDR_reset_n,
+    DDR_we_n => DDR_we_n,
+    FIXED_IO_ddr_vrn => FIXED_IO_ddr_vrn,
+    FIXED_IO_ddr_vrp => FIXED_IO_ddr_vrp,
+    FIXED_IO_mio(53 downto 0) => FIXED_IO_mio(53 downto 0),
+    FIXED_IO_ps_clk => FIXED_IO_ps_clk,
+    FIXED_IO_ps_porb => FIXED_IO_ps_porb,
+    FIXED_IO_ps_srstb => FIXED_IO_ps_srstb,
+    dshot_0 => dshot_0,
+    dshot_1 => dshot_1,
+    dshot_2 => dshot_2,
+    dshot_3 => dshot_3,
+    iic_imu_scl_i => iic_imu_scl_i,
+    iic_imu_scl_o => iic_imu_scl_o,
+    iic_imu_scl_t => iic_imu_scl_t,
+    iic_imu_sda_i => iic_imu_sda_i,
+    iic_imu_sda_o => iic_imu_sda_o,
+    iic_imu_sda_t => iic_imu_sda_t
+  );
+    
+  iic_imu_scl_iobuf: component IOBUF
+  port map (
+    I  => iic_imu_scl_o,
+    IO => iic_imu_scl,
+    O  => iic_imu_scl_i,
+    T  => iic_imu_scl_t
+  );
+
+  iic_imu_sda_iobuf: component IOBUF
+  port map (
+    I  => iic_imu_sda_o,
+    IO => iic_imu_sda,
+    O  => iic_imu_sda_i,
+    T  => iic_imu_sda_t
+  );
+    
 end STRUCTURE;
--- _fpga/xdc/toplevel.xdc
+++ _fpga/xdc/toplevel.xdc
@@ -1,4 +1,4 @@
-# dshot
+# DShot
 set_property PACKAGE_PIN H20 [get_ports dshot_0]
 set_property PACKAGE_PIN J20 [get_ports dshot_1]
 set_property PACKAGE_PIN G20 [get_ports dshot_2]
@@ -7,3 +7,9 @@ set_property IOSTANDARD LVCMOS33 [get_ports dshot_0]
 set_property IOSTANDARD LVCMOS33 [get_ports dshot_1]
 set_property IOSTANDARD LVCMOS33 [get_ports dshot_2]
 set_property IOSTANDARD LVCMOS33 [get_ports dshot_3]
+
+# IIC IMU
+set_property PACKAGE_PIN W20 [get_ports iic_imu_scl]
+set_property PACKAGE_PIN V20 [get_ports iic_imu_sda]
+set_property IOSTANDARD LVCMOS33 [get_ports iic_imu_scl]
+set_property IOSTANDARD LVCMOS33 [get_ports iic_imu_sda]
