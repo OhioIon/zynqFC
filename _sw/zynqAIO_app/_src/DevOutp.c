d196c3dcd131e41c3021789634f3ccd1871aa63f
OhioIon <->
2019-07-11 22:50:44 +0200

    Fixing build

--- _sw/zynqAIO_app/_src/DevOutp.c
+++ _sw/zynqAIO_app/_src/DevOutp.c
@@ -1,13 +1,12 @@
 #define DEVOUTP_C
 
 /****************** Includes ********************/
-
-
 #include "DevOutp.h"
 #include "DevInp.h"
 #include "Veh.h"
-#include "xparameters.h"
-#include "xil_printf.h"
+
+#include <xparameters.h>
+#include <xil_printf.h>
 
 /******************* Defines ********************/
 
@@ -30,16 +29,16 @@ uint8_t DevOutp_init( void )
   retVal_u8 += dshotWaveform_init( &DevOutp_s.dshotWaveform_s );
 
   // Init motor DSHOT protocol drivers
-  DevOutp_s.dshotMotFrntLeft_s.prm_s.addrBas_pv = (void*)XPAR_DSHOT_1_0;
+  DevOutp_s.dshotMotFrntLeft_s.prm_s.addrBas_pv = (void*)XPAR_DSHOT_0_0;
   retVal_u8 += dshot_init( &DevOutp_s.dshotMotFrntLeft_s );
 
-  DevOutp_s.dshotMotFrntRght_s.prm_s.addrBas_pv = (void*)XPAR_DSHOT_2_0;
+  DevOutp_s.dshotMotFrntRght_s.prm_s.addrBas_pv = (void*)XPAR_DSHOT_1_0;
   retVal_u8 += dshot_init( &DevOutp_s.dshotMotFrntRght_s );
 
-  DevOutp_s.dshotMotRearLeft_s.prm_s.addrBas_pv = (void*)XPAR_DSHOT_3_0;
+  DevOutp_s.dshotMotRearLeft_s.prm_s.addrBas_pv = (void*)XPAR_DSHOT_2_0;
   retVal_u8 += dshot_init( &DevOutp_s.dshotMotRearLeft_s );
 
-  DevOutp_s.dshotMotRearRght_s.prm_s.addrBas_pv = (void*)XPAR_DSHOT_4_0;
+  DevOutp_s.dshotMotRearRght_s.prm_s.addrBas_pv = (void*)XPAR_DSHOT_3_0;
   retVal_u8 += dshot_init( &DevOutp_s.dshotMotRearRght_s );
 
   return retVal_u8;
@@ -52,10 +51,10 @@ void DevOutp( void )
   if( (Veh_s.outp_s.flgArmed_u8 != 0) && (DevInp_s.outp_s.flgLost_u8 == 0) )
   {
     // Output desired throttle values via DSHOT to motor ESCs
-    DevOutp_s.dshotMotFrntLeft_s.inp_s.rawData_u16 = Veh_s.outp_s.rawDataFrntLeft_u16;
-    DevOutp_s.dshotMotFrntRght_s.inp_s.rawData_u16 = Veh_s.outp_s.rawDataFrntRght_u16;
-    DevOutp_s.dshotMotRearLeft_s.inp_s.rawData_u16 = Veh_s.outp_s.rawDataRearLeft_u16;
-    DevOutp_s.dshotMotRearRght_s.inp_s.rawData_u16 = Veh_s.outp_s.rawDataRearRght_u16;
+    DevOutp_s.dshotMotFrntLeft_s.inp_s.thrData_u16 = Veh_s.outp_s.rawDataFrntLeft_u16;
+    DevOutp_s.dshotMotFrntRght_s.inp_s.thrData_u16 = Veh_s.outp_s.rawDataFrntRght_u16;
+    DevOutp_s.dshotMotRearLeft_s.inp_s.thrData_u16 = Veh_s.outp_s.rawDataRearLeft_u16;
+    DevOutp_s.dshotMotRearRght_s.inp_s.thrData_u16 = Veh_s.outp_s.rawDataRearRght_u16;
     dshot( &DevOutp_s.dshotMotFrntLeft_s );
     dshot( &DevOutp_s.dshotMotFrntRght_s );
     dshot( &DevOutp_s.dshotMotRearLeft_s );
