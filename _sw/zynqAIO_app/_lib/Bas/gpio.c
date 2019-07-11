3bdbe4141c2a5dc1ef9a46f67f4a5e55c0280ab1
OhioIon <->
2019-07-11 22:41:14 +0200

    Added BTN5 and some INT pins

--- _sw/zynqAIO_app/_lib/Bas/gpio.c
+++ _sw/zynqAIO_app/_lib/Bas/gpio.c
@@ -1,46 +1,62 @@
 // Includes
-#include <xparameters.h>
-#include <xgpiops.h>
+#include "gpio.h"
 
 // Global Variables
-static XGpioPs inst_s;
+static XGpioPs XGpioPs_s;
 
 uint8_t gpio_init( void )
 {
-	int retVal;
+  int retVal;
 
-	// Load PS-GPIO core config and init hardware
-	XGpioPs_Config *cfg_ps = XGpioPs_LookupConfig( XPAR_PS7_GPIO_0_DEVICE_ID );
-	retVal = XGpioPs_CfgInitialize( &inst_s, cfg_ps, cfg_ps->BaseAddr );
-	if( retVal != XST_SUCCESS ) return 1;
+  // Get configuration
+  XGpioPs_Config *cfg_ps = XGpioPs_LookupConfig( XPAR_PS7_GPIO_0_DEVICE_ID );
+  if( NULL == cfg_ps ) return 1;
 
-	// Make pin MIO7 (LED LD4 on Zybo) output
-	XGpioPs_SetDirectionPin(&inst_s, 7, 1 );
-	XGpioPs_SetOutputEnablePin(&inst_s, 7, 1);
+  // Initialize hardware/driver
+  retVal = XGpioPs_CfgInitialize( &XGpioPs_s, cfg_ps, cfg_ps->BaseAddr );
+  if( retVal != XST_SUCCESS ) return 1;
 
-  // Make pin MIO14 (CE pin on nRF24L01 transceiver) output
-  XGpioPs_SetDirectionPin( &inst_s, 14, 1 );
-  XGpioPs_SetOutputEnablePin(&inst_s, 14, 1);
+  // Make pin MIO7 (LED LD4 on Zybo) output
+  XGpioPs_SetDirectionPin(&XGpioPs_s, 7, 1 );
+  XGpioPs_SetOutputEnablePin(&XGpioPs_s, 7, 1);
+
+  // Make pin MIO14 (CE pin on nRF24L01) output
+  XGpioPs_SetDirectionPin( &XGpioPs_s, 14, 1 );
+  XGpioPs_SetOutputEnablePin(&XGpioPs_s, 14, 1);
+
+  // Make pin MIO15 (IRQ pin on nRF24L01) input
+  XGpioPs_SetDirectionPin( &XGpioPs_s, 15, 0 );
 
   // Make pin MIO50 (BTN4 on Zybo) input
-  XGpioPs_SetDirectionPin( &inst_s, 50, 0 );
+  XGpioPs_SetDirectionPin( &XGpioPs_s, 50, 0 );
 
-	// Done
-	return 0;
+  // Make pin MIO51 (BTN5 on Zybo) input
+  XGpioPs_SetDirectionPin( &XGpioPs_s, 51, 0 );
+
+  // Make pin MIO54 (EMIO) (INT pin on MPU-6000) input
+  XGpioPs_SetDirectionPin( &XGpioPs_s, 54, 0 );
+
+  // Done
+  return 0;
 }
 
 u8 gpio_get_BTN4( void )
 {
-  return (u8)XGpioPs_ReadPin( &inst_s, 50 );
+  return (u8)XGpioPs_ReadPin( &XGpioPs_s, 50 );
+}
+
+u8 gpio_get_BTN5( void )
+{
+  return (u8)XGpioPs_ReadPin( &XGpioPs_s, 51 );
 }
 
 void gpio_set_LD4( uint8_t on )
 {
-	XGpioPs_WritePin( &inst_s, 7, (on != 0) );
+	XGpioPs_WritePin( &XGpioPs_s, 7, (on != 0) );
 }
 
 void gpio_nRF24L01_CE( uint8_t on )
 {
-  XGpioPs_WritePin( &inst_s, 14, (on != 0) );
+  XGpioPs_WritePin( &XGpioPs_s, 14, (on != 0) );
 }
 
--- _sw/zynqAIO_app/_lib/Bas/gpio.h
+++ _sw/zynqAIO_app/_lib/Bas/gpio.h
@@ -7,6 +7,7 @@
 int gpio_init( void );
 
 uint8_t gpio_get_BTN4( void );
+uint8_t gpio_get_BTN5( void );
 void gpio_set_LD4( uint8_t on_u8 );
 void gpio_nRF24L01_CE( uint8_t on_u8 );
 
