dd64b987dd94b53ba95b2925452eb817afd8eda4
OhioIon <->
2019-08-08 11:38:25 +0200

    Cleaned up gpio module

--- _sw/app/_lib/Bas/gpio.c
+++ _sw/app/_lib/Bas/gpio.c
@@ -1,66 +1,112 @@
-// Includes
+#define GPIO_C
+
+/****************** Includes ********************/
+
 #include "gpio.h"
 
 #include <xgpiops.h>
 
-// Global Variables
+/******************* Defines ********************/
+
+// MIO PIN numbers
+#define LD4             7
+#define nRF24L01_CE    14
+#define nRF24L01_IRQ   15
+#define BTN4           50
+#define BTN5           51
+
+// EMIO PIN numbers
+#define mpu6000_INT    54
+
+/******************** Types *********************/
+
+/***************** Private Data *****************/
+
 static XGpioPs XGpioPs_s;
+
+/****************** Prototypes ******************/
+
+/**************** Implementation ****************/
 
 uint8_t gpio_init( void )
 {
   int retVal;
 
-  // Get configuration
-  XGpioPs_Config *cfg_ps = XGpioPs_LookupConfig( XPAR_PS7_GPIO_0_DEVICE_ID );
-  if( NULL == cfg_ps ) return 1;
+  /////////////////// PS-GPIO ///////////////////
+  {
+    // Initialize hardware/driver
+    XGpioPs_Config *cfg_ps = XGpioPs_LookupConfig( XPAR_PS7_GPIO_0_DEVICE_ID );
+    if( NULL == cfg_ps ) return 1;
+    retVal = XGpioPs_CfgInitialize( &XGpioPs_s, cfg_ps, cfg_ps->BaseAddr );
+    if( retVal != XST_SUCCESS ) return 1;
 
-  // Initialize hardware/driver
-  retVal = XGpioPs_CfgInitialize( &XGpioPs_s, cfg_ps, cfg_ps->BaseAddr );
-  if( retVal != XST_SUCCESS ) return 1;
+    // Self-test
+    retVal = XGpioPs_SelfTest( &XGpioPs_s );
+    if( retVal != XST_SUCCESS ) return 1;
 
-  // Make pin MIO7 (LED LD4 on Zybo) output
-  XGpioPs_SetDirectionPin(&XGpioPs_s, 7, 1 );
-  XGpioPs_SetOutputEnablePin(&XGpioPs_s, 7, 1);
-  XGpioPs_WritePin( &XGpioPs_s, 7, 0 );
+    ///// MIO Pins /////
 
-  // Make pin MIO14 (CE pin on nRF24L01) output
-  XGpioPs_SetDirectionPin( &XGpioPs_s, 14, 1 );
-  XGpioPs_SetOutputEnablePin(&XGpioPs_s, 14, 1);
-  XGpioPs_WritePin( &XGpioPs_s, 14, 0 );
+    // Make pin MIO7 (LED LD4 on Zybo) output
+    XGpioPs_SetDirectionPin(&XGpioPs_s, LD4, 1 );
+    XGpioPs_SetOutputEnablePin(&XGpioPs_s, LD4, 1);
+    XGpioPs_WritePin( &XGpioPs_s, LD4, 0 );
 
-  // Make pin MIO15 (IRQ pin on nRF24L01) input
-  XGpioPs_SetDirectionPin( &XGpioPs_s, 15, 0 );
+    // Make pin MIO14 (CE pin on nRF24L01) output
+    XGpioPs_SetDirectionPin( &XGpioPs_s, nRF24L01_CE, 1 );
+    XGpioPs_SetOutputEnablePin(&XGpioPs_s, nRF24L01_CE, 1);
+    XGpioPs_WritePin( &XGpioPs_s, nRF24L01_CE, 0 );
 
-  // Make pin MIO50 (BTN4 on Zybo) input
-  XGpioPs_SetDirectionPin( &XGpioPs_s, 50, 0 );
+    // Make pin MIO15 (IRQ pin on nRF24L01) input
+    XGpioPs_SetDirectionPin( &XGpioPs_s, nRF24L01_IRQ, 0 );
 
-  // Make pin MIO51 (BTN5 on Zybo) input
-  XGpioPs_SetDirectionPin( &XGpioPs_s, 51, 0 );
+    // Make pin MIO50 (BTN4 on Zybo) input
+    XGpioPs_SetDirectionPin( &XGpioPs_s, BTN4, 0 );
 
-  // Make pin MIO54 (EMIO) (INT pin on MPU-6000) input
-  XGpioPs_SetDirectionPin( &XGpioPs_s, 54, 0 );
+    // Make pin MIO51 (BTN5 on Zybo) input
+    XGpioPs_SetDirectionPin( &XGpioPs_s, BTN5, 0 );
+
+    ///// EMIO Pins /////
+
+    // Make pin MIO54 (INT pin on MPU-6000) input
+    XGpioPs_SetDirectionPin( &XGpioPs_s, mpu6000_INT, 0 );
+  }
 
   // Done
   return 0;
 }
 
-uint8_t gpio_get_BTN4( void )
+///// MIO Pins /////
+
+void gpio_set_LD4( bool on )
 {
-  return (uint8_t)XGpioPs_ReadPin( &XGpioPs_s, 50 );
+  XGpioPs_WritePin( &XGpioPs_s, LD4, on );
 }
 
-uint8_t gpio_get_BTN5( void )
+void gpio_nRF24L01_CE( bool on )
 {
-  return (uint8_t)XGpioPs_ReadPin( &XGpioPs_s, 51 );
+  XGpioPs_WritePin( &XGpioPs_s, nRF24L01_CE, on );
 }
 
-void gpio_set_LD4( uint8_t on )
+bool gpio_nRF24L01_IRQ( void )
 {
-	XGpioPs_WritePin( &XGpioPs_s, 7, (on != 0) );
+  return (bool)XGpioPs_ReadPin( &XGpioPs_s, nRF24L01_IRQ );
 }
 
-void gpio_nRF24L01_CE( uint8_t on )
+bool gpio_get_BTN4( void )
 {
-  XGpioPs_WritePin( &XGpioPs_s, 14, (on != 0) );
+  return (bool)XGpioPs_ReadPin( &XGpioPs_s, BTN4 );
 }
 
+bool gpio_get_BTN5( void )
+{
+  return (bool)XGpioPs_ReadPin( &XGpioPs_s, BTN5 );
+}
+
+///// EMIO Pins /////
+
+bool gpio_mpu6000_INT( void )
+{
+  return (bool)XGpioPs_ReadPin( &XGpioPs_s, mpu6000_INT );
+}
+
+// EOF
--- _sw/app/_lib/Bas/gpio.h
+++ _sw/app/_lib/Bas/gpio.h
@@ -1,13 +1,35 @@
 #ifndef GPIO_H_
 #define GPIO_H_
 
+#ifdef GPIO_C
+#define GPIO_SCOPE
+#else
+#define GPIO_SCOPE extern
+#endif
+
+/****************** Includes ********************/
+
 #include <stdint.h>
+#include <stdbool.h>
 
-uint8_t gpio_init( void );
+/******************* Defines ********************/
 
-uint8_t gpio_get_BTN4( void );
-uint8_t gpio_get_BTN5( void );
-void gpio_set_LD4( uint8_t on_u8 );
-void gpio_nRF24L01_CE( uint8_t on_u8 );
+/******************** Types *********************/
+
+/****************** Public Data *****************/
+
+/******************* Interface ******************/
+
+GPIO_SCOPE uint8_t gpio_init( void );
+
+// MIO Pins
+GPIO_SCOPE void gpio_set_LD4( bool on_l );
+GPIO_SCOPE void gpio_nRF24L01_CE( bool on_l );
+GPIO_SCOPE bool gpio_nRF24L01_IRQ( void );
+GPIO_SCOPE bool gpio_get_BTN4( void );
+GPIO_SCOPE bool gpio_get_BTN5( void );
+
+// EMIO Pins
+GPIO_SCOPE bool gpio_mpu6000_INT( void );
 
 #endif /* GPIO_H_ */
